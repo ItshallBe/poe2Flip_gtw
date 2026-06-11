7dab81fdb2acdb56365f3c4cfac77ce82dd7ead8
ItshallBe <916038895@qq.com>
2026-06-11 17:23:37 +0800

    fix<main>: 解决包导入的问题

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import bodyParser from 'body-parser';
-import compression from 'compression';
 import { AppModule } from './app.module';
 import { installProductionDiagnostics } from './diagnostics/production-diagnostics';
+const compression = require('compression');
+const bodyParser = require('body-parser');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
